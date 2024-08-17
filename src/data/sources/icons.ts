d953769b87a6b17bc092393efa8efbee3f80ca9a
Cedric Angulo <cdrcangulo@gmail.com>
2024-08-17 16:08:45 +0800

    added resources

--- src/data/sources/icons.ts
+++ src/data/sources/icons.ts
@@ -35,6 +35,34 @@ export const icons: Resource[] = [
 			'Tabler Icons: 5467 free and open source SVG icons. Customizable size, color and stroke. Available for React, SolidJS, Vue, Figma and more for free!',
 		icon: '/resources/icons/tabler.png',
 	},
+	{
+		name: 'Streamline',
+		url: 'https://www.streamlinehq.com/',
+		description:
+			'8 icon designers rebels. Creating from scratch innovative and harmonious icons sets.',
+		icon: '/resources/icons/streamline.png',
+	},
+	{
+		name: 'Iconoir',
+		url: 'https://www.streamlinehq.com/',
+		description:
+			'Iconoir is the biggest open source icon library that provides a massive selection of high-quality icons, available for free download. No premium options or email sign-up required, free for real. Icons available in SVG, Font, React, React Nativ, and Flutter libraries, Figma and Framer.',
+		icon: '/resources/icons/iconoir.png',
+	},
+	{
+		name: 'Line Icons',
+		url: 'https://lineicons.com/icons',
+		description:
+			'Explore 8400+ Free & Premium Professionally Designed SVG Icons: Enhance your projects with our vast collection of SVG UI Icons from 40+ categories. Download in various formats - SVG, Web Font, Figma, React, PNG & More.',
+		icon: '/resources/icons/line-icons.png',
+	},
+	{
+		name: 'Feather Icons',
+		url: 'https://feathericons.com/',
+		description:
+			'Feather is a collection of simply beautiful open source icons. Each icon is designed on a 24x24 grid with an emphasis on simplicity, consistency and readability.',
+		icon: '/resources/icons/feathericons.png',
+	},
 	// {
 	// 	name: '',
 	// 	url: '',
--- src/data/sources/typography.ts
+++ src/data/sources/typography.ts
@@ -2,15 +2,37 @@ import { Resource } from '@/lib/types';
 
 export const typography: Resource[] = [
 	{
+		name: 'FontFrabric',
+		url: 'https://www.fontspace.com',
+		description:
+			'A digital type foundry crafting retail fonts and custom typography for various brands. Sharing a passion for premium typefaces, calligraphy and lettering.',
+		icon: '/resources/typography/fontfabric.png',
+	},
+	{
+		name: 'FontSpace',
+		url: 'https://www.fontspace.com',
+		description:
+			'Free downloads of legally licensed fonts that are perfect for your design projects. The best place in the universe to search for amazing fonts.',
+		icon: '/resources/typography/fontspace.png',
+	},
+	{
+		name: 'DaFont',
+		url: 'https://www.dafont.com/',
+		description:
+			'Archive of freely downloadable fonts. Browse by alphabetical listing, by style, by author or by popularity.',
+		icon: '/resources/typography/dafont.png',
+	},
+	{
 		name: 'Google Fonts',
 		url: 'https://fonts.google.com',
 		description: 'A vast collection of open-source fonts provided by Google.',
-		icon: '/resources/colors/google-fonts.svg',
+		icon: '/resources/typography/google-fonts.svg',
 	},
 	{
 		name: 'Font Squirrel',
 		url: 'https://www.fontsquirrel.com',
-		description: 'A collection of free, high-quality fonts for commercial use.',
-		icon: '/font-squirrel.png',
+		description:
+			'Font Squirrel scours the internet for high quality, legitimately free fonts . Download thousands of completely legal, high quality, free fonts.',
+		icon: '/resources/typography/fontsquirrel.png',
 	},
 ];
