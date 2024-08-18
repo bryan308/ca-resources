7d6bfbf1981ead19f1d55e302497bbf829890e67
Cedric Angulo <cdrcangulo@gmail.com>
2024-08-18 15:05:32 +0800

    added suppressHydrationWarning and minimum height in main tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,14 +16,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
+		<html
+			lang='en'
+			suppressHydrationWarning
+		>
 			<body
 				className={`${geistsans.className} bg-[#FAFAFA] dark:bg-black text-[#171717] dark:text-[#EDEDED]`}
 			>
 				<Providers>
 					<Header />
 					<div className='h-16 md:hidden' />
-					<main className='flex max-w-[76.25rem] flex-col xl:mx-auto xl:mt-0 xl:grid xl:grid-cols-[16.25rem_1fr]'>
+					<main className='flex max-w-[76.25rem] flex-col xl:mx-auto xl:mt-0 xl:grid xl:grid-cols-[16.25rem_1fr] min-h-[90dvh]'>
 						<Aside />
 						<div className='border border-t-0 border-l-0 primary-border h-full'>{children}</div>
 					</main>
