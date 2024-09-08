91b7184fc55f908dca1c3e0113638469e25855c0
Cedric Angulo <cdrcangulo@gmail.com>
2024-09-08 19:04:48 +0800

    new feature scrolltotop button and colors

--- src/components/shared/ui/scroll-to-top.tsx
+++ src/components/shared/ui/scroll-to-top.tsx
@@ -1,15 +1,28 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { ChevronUpIcon } from '@radix-ui/react-icons';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const ScrollToTopButton = () => {
 	const [shown, setShown] = useState(false);
+	const hideTimeout = useRef<NodeJS.Timeout | null>(null);
 
 	useEffect(() => {
 		const scrollCallback = () => {
 			const scrolledFromTop = window.scrollY;
-			setShown(() => scrolledFromTop > 300);
+
+			if (hideTimeout.current) clearTimeout(hideTimeout.current);
+
+			if (scrolledFromTop > 300) {
+				setShown(true);
+
+				hideTimeout.current = setTimeout(() => {
+					setShown(false);
+				}, 5000);
+			} else {
+				setShown(false);
+			}
 		};
 
 		window.addEventListener('scroll', scrollCallback);
@@ -18,6 +31,7 @@ const ScrollToTopButton = () => {
 
 		return () => {
 			window.removeEventListener('scroll', scrollCallback);
+			if (hideTimeout.current) clearTimeout(hideTimeout.current);
 		};
 	}, []);
 
@@ -28,12 +42,15 @@ const ScrollToTopButton = () => {
 	return (
 		<button
 			aria-label='scroll to top'
+			title='scroll to top'
 			onClick={scrolltoTop}
-			className={`${
-				shown ? 'scale-100' : 'scale-0'
-			} w-12 h-12 transition-transform duration-200 flex fixed right-6 xl:right-12 xl:bottom-8 bottom-32 bg-card rounded-full justify-center items-center`}
+			className={cn(
+				shown ? 'scale-100' : 'scale-0',
+				'w-12 h-12 transition-transform duration-200 flex fixed right-6 xl:right-12 xl:bottom-8 bottom-32',
+				'bg-card border-2 border-border-foreground rounded-full justify-center items-center'
+			)}
 		>
-			<ChevronUpIcon className='size-6' />
+			<ChevronUpIcon className='size-6 text-secondary' />
 		</button>
 	);
 };
