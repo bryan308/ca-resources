458df7bf7c608239a9646c8448a79d6d32ac889f
“bryan-308” <supahhotboi11@gmail.com>
2024-09-18 23:56:20 +0800

    fixed wrong fetch :<

--- src/components/shared/contributors.tsx
+++ src/components/shared/contributors.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import Link from "next/link"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { cn } from "@/lib/utils"
+import { useEffect, useState } from "react"
+import { Skeleton } from "../ui/skeleton"
 
 interface Contributor {
 	id: number
@@ -9,42 +13,78 @@ interface Contributor {
 	avatar_url: string
 }
 
-async function fetchContributors(): Promise<Contributor[]> {
-	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/contributors`, {
-		headers: {
-			Accept: "application/vnd.github+json",
-		},
-	})
-	if (!response.ok) {
-		throw new Error("Failed to fetch contributors")
-	}
-	return response.json()
-}
+// ! async function fetchContributors(): Promise<Contributor[]> {
+// ! 	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/contributors`, {
+// ! 		headers: {
+// ! 			Accept: "application/vnd.github+json",
+// ! 		},
+// ! 	})
+// ! 	if (!response.ok) {
+// ! 		throw new Error("Failed to fetch contributors")
+// ! 	}
+// ! 	return response.json()
+// ! }
 
-export default async function Contributors({ className }: { className?: string }) {
-	const contributors = await fetchContributors()
+export default function Contributors({ className }: { className?: string }) {
+	// ! const contributors = await fetchContributors()
+
+	const [contributors, setContributors] = useState<Contributor[]>([])
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const fetchContributors = async () => {
+			try {
+				const response = await fetch("/api/contributors")
+				const data = await response.json()
+				setContributors(data)
+			} catch (error) {
+				console.error("Error fetching contributors:", error)
+			} finally {
+				setLoading(false)
+			}
+		}
+
+		fetchContributors()
+	}, [])
 
 	return (
 		<div className={cn(className, "flex flex-wrap gap-2")}>
-			{contributors.map((contributor: Contributor) => (
-				<div key={contributor.id}>
-					{contributor.login !== "bryan308" && (
-						<Avatar>
-							<Link
-								href={contributor.html_url}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<AvatarImage
-									src={contributor.avatar_url}
-									alt={contributor.login}
-								/>
-								<AvatarFallback className="text-xs">{contributor.login}</AvatarFallback>
-							</Link>
-						</Avatar>
-					)}
-				</div>
-			))}
+			{loading ? (
+				<>
+					{/* TODO: Dynamic skeleton count based on the total contributors */}
+					{Array(2)
+						.fill(null)
+						.map((_, index) => (
+							<Skeleton
+								key={index}
+								className="size-10 rounded-full"
+							/>
+						))}
+				</>
+			) : (
+				<>
+					{contributors.length === 0 && <p>No contributors found.</p>}
+					{contributors.map((contributor: Contributor) => (
+						<div key={contributor.id}>
+							{contributor.login !== "bryan308" && (
+								<Avatar>
+									<Link
+										href={contributor.html_url}
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<AvatarImage
+											src={contributor.avatar_url}
+											alt={contributor.login}
+										/>
+										<AvatarFallback className="text-xs">{contributor.login}</AvatarFallback>
+									</Link>
+								</Avatar>
+							)}
+						</div>
+					))}
+				</>
+			)}
 		</div>
 	)
 }
