abe158b1d51c6b304e8ec8f8025a47a1bc758703
bryan308 <cdrcangulo@gmail.com>
2025-01-04 15:16:45 +0800

    feat: organize mdx components imports and update contribution guide

--- components/shared/mdx-components.tsx
+++ components/shared/mdx-components.tsx
@@ -1,60 +1,60 @@
+import { EmailGenerator } from "./gen-email"
+import { ImageZoom } from "fumadocs-ui/components/image-zoom"
+import { Step, Steps } from "fumadocs-ui/components/steps"
+import { Tab, Tabs } from "fumadocs-ui/components/tabs"
+import { File, Folder, Files } from "fumadocs-ui/components/files"
+import defaultMdxComponents from "fumadocs-ui/mdx"
+import { LoginForm } from "@/components/shared/login-form"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "../ui/button"
+import { Checkbox } from "../ui/checkbox"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { Slider } from "../ui/slider"
+import CustomCard from "./resources/custom-card"
+import ImageSection from "./image-section"
+import { Wrapper } from "./wrapper"
 import {
 	BorderExample,
 	DataTable,
 	DataTableExample,
-	SummaryTable,
 	OrderedList,
+	SummaryTable,
 	TableCellSpanning,
 	TableExample,
 	UnorderedList,
 } from "./mdx-elements"
-import defaultMdxComponents from "fumadocs-ui/mdx"
-import { ImageZoom } from "fumadocs-ui/components/image-zoom"
-import { Step, Steps } from "fumadocs-ui/components/steps"
-import { LoginForm } from "@/components/shared/login-form"
-import { Tab, Tabs } from "fumadocs-ui/components/tabs"
-import { File, Folder, Files } from "fumadocs-ui/components/files"
-import { EmailGenerator } from "./gen-email"
-import { Button } from "../ui/button"
-import { Wrapper } from "./wrapper"
-import { Label } from "../ui/label"
-import { Input } from "../ui/input"
-import { Checkbox } from "../ui/checkbox"
-import { Slider } from "../ui/slider"
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
-import { Textarea } from "@/components/ui/textarea"
-import ImageSection from "./image-section"
-import CustomCard from "./resources/custom-card"
 
 export const components = {
-	CustomCard,
 	BorderExample,
+	Button,
+	Checkbox,
+	CustomCard,
 	DataTable,
 	DataTableExample,
 	EmailGenerator,
-	OrderedList,
-	UnorderedList,
-	TableCellSpanning,
-	TableExample,
-	LoginForm,
-	SummaryTable,
-	Button,
-	Checkbox,
 	File,
-	Folder,
 	Files,
+	Folder,
+	Image: (props: React.ComponentProps<typeof ImageZoom>) => <ImageZoom {...props} />,
 	ImageSection,
 	Input,
 	Label,
+	LoginForm,
+	OrderedList,
 	RadioGroup,
 	RadioGroupItem,
 	Slider,
-	Steps,
 	Step,
-	Textarea,
+	Steps,
+	SummaryTable,
 	Tab,
 	Tabs,
+	TableCellSpanning,
+	TableExample,
+	Textarea,
+	UnorderedList,
 	Wrapper,
-	Image: (props: React.ComponentProps<typeof ImageZoom>) => <ImageZoom {...props} />,
 	...defaultMdxComponents,
 }
