797f99bc9504506dce60b189641cd999c713f73d
bryan308 <cdrcangulo@gmail.com>
2025-01-07 15:26:55 +0800

    fix: update background classes for consistency in global styles and resources marquee

--- components/shared/home/resources-marquee.tsx
+++ components/shared/home/resources-marquee.tsx
@@ -113,8 +113,8 @@ export function MarqueeDemo() {
 					/>
 				))}
 			</Marquee>
-			<div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-background"></div>
-			<div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-background"></div>
+			<div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-fd-background"></div>
+			<div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-fd-background"></div>
 		</div>
 	)
 }
